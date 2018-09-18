6d8da0e177edc867ee6bdce8052abfc88ed9ed40
kasanari <jakobnnyberg@gmail.com>
2018-09-18 13:25:38 +0200

    implemented cyclic buffer

--- comm1.adb
+++ comm1.adb
@@ -20,13 +20,42 @@ procedure comm1 is
             -- add your task entries for communication 
 	end consumer;
 
-	task body buffer is 
+   task body buffer is -- Cyclic buffer code from https://en.wikibooks.org/wiki/Ada_Programming/Tasking
 		Message: constant String := "buffer executing";
-                -- change/add your local declarations here   
+      Q_Size : constant := 10;
+      subtype Q_Range is Positive range 1 .. Q_Size;
+      Length : Natural range 0 .. Q_Size := 0;
+      Head, Tail : Q_Range := 1;
+      Data : array (Q_Range) of Integer;
+      Kill_Flag: Boolean := False;
 	begin
 		Put_Line(Message);
 		loop
-                -- add your task code inside this loop    
+	 select
+	    when Length < Q_Size =>
+	       accept Insert (number : in Integer) do
+		  Data(Tail) := number;
+	       end Insert;
+	       Tail := Tail mod Q_Size + 1;
+	       Length := Length + 1;
+	 or
+	    when Length > 0 =>
+	       accept Remove (number : out Integer) do
+		  number := Data(Head);
+	       end Remove;
+	       Head := Head mod Q_Size + 1;
+	       Length := Length - 1;
+	 or
+	    accept Kill do 
+	       Kill_Flag := True;
+	    end Kill;
+	 end select;
+	 
+	 if (Kill_Flag) then
+	    Put_Line("buffer was killed");
+	    exit;
+	 end if;
+
 		end loop;
 	end buffer;
 
